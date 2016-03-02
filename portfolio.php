eb38579a324da7a8d149a305689e0afa1ee04241
ojoonwoo <ojoonwoo@gmail.com>
2016-03-02 17:22:37 +0900

    url update

--- portfolio.php
+++ portfolio.php
@@ -1,17 +1,17 @@
 <?php
 $id = $_GET[pid];
 if($id=="p1") {
-  echo "<object type='application/pdf' data='http://192.168.219.163:8080/portfolio/img/portfolio1.pdf' width='100%' height='800'>
+  echo "<object type='application/pdf' data='https://ojoonwoo.github.io/img/portfolio1.pdf' width='100%' height='800'>
   <param name='src' value='portfolio1.pdf'>
   </object>";
 }
 else if($id=="p2") {
-  echo "<object type='application/pdf' data='http://192.168.219.163:8080/portfolio/img/portfolio2.pdf' width='100%' height='800'>
+  echo "<object type='application/pdf' data='https://ojoonwoo.github.io/img/portfolio2.pdf' width='100%' height='800'>
   <param name='src' value='portfolio2.pdf'>
   </object>";
 }
 else if($id=="p3"){
-  echo "<object type='application/pdf' data='http://192.168.219.163:8080/portfolio/img/portfolio3.pdf' width='100%' height='800'>
+  echo "<object type='application/pdf' data='https://ojoonwoo.github.io/img/portfolio3.pdf' width='100%' height='800'>
   <param name='src' value='portfolio3.pdf'>
   </object>";
 }
